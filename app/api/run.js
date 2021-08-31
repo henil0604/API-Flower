0e6e1cfcc9daca066a760b87375ff87433fca11a
henil0604 <henilmalaviya06@gmail.com>
2021-08-31 20:07:29 +0530

    IP Fixed

--- app/api/run.js
+++ app/api/run.js
@@ -9,7 +9,7 @@ function createSandBox(req, res) {
     sandBox.req = { };
     sandBox.sandbox = { };
 
-    sandBox.req.ip = req.ip;
+    sandBox.req.ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
     sandBox.req.method = req.method;
     sandBox.req.originalUrl = req.originalUrl;
     sandBox.req.params = req.params;
